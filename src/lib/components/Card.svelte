cf9368de7e6f37bf9807bb358c341012ae335e17
Brendan Malycky <bmalycky@hotmail.com>
2023-11-06 08:09:48 -0700

    created card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let heading = "placeholder heading";
+  export let description = "placeholder description";
+</script>
+
+<article class="max-w-fit bg-orange-200 rounded-xl p-1">
+  <div class="w-32 h-32 bg-black mx-auto rounded-xl my-2"></div>
+  <h2>{heading}</h2>
+  <p>{description}</p>
+</article>
